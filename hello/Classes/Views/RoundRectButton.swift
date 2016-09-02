645b18930cdc92d8f5b5a366669624d41737d5f3
sun <shun.nishitsuji@pepabo.com>
2016-09-02 16:40:57 +0900

    create roundrectbutton

---
+++ hello/Classes/Views/RoundRectButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundRectButton.swift
+//  hello
+//
+//  Created by usr0600439 on 2016/09/02.
+//  Copyright © 2016年 GMO Pepabo. All rights reserved.
+//
+
+import UIKit
+
+class RoundRectButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
